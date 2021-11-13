2101cb676cd60acd51059db59868527c285f9079
Femi Akinbode <femi661@gmail.com>
2021-11-13 07:52:02 +0100

    Add project files.

---
+++ Repository/IHttpClientHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace torreAssesment.Repository
+{
+    interface IHttpClientHelper<T>
+    {
+        Task<T> Get(Uri uri, string query);
+        Task<T> Post(Uri uri, string payload);
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace torreAssesment.Repository
+{
+    public interface IRepository<T1, T2>
+    {
+        Task<T1> Get(T2 query);
+        Task<T1> Post(T2 payload);        
+    }
+}
